c4d85abed655dee1dc04fccb652c7a7d288149d9
blessedelectric303-maker <blessedelectric303@gmail.com>
2026-07-07 10:52:29 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
+import { isStaff, homeFor } from "@/utils/roles";
 
 type AppRow = {
   key: string;
